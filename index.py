eca507ab0804c9a1ce8517ddf3968b6247b8b050
Leojb2oo8 <118618337+Leojb2oo8@users.noreply.github.com>
2024-12-11 11:12:00 +0000

    menu changes

--- index.py
+++ index.py
@@ -22,7 +22,7 @@ def varMod():
     if choice == "1":
         variableName = input("What do you whant the variable to be named:\n>> ")
         data = input("What do you whant the variable data to be:\n>> ")
-        code.append(["input", len(inputData)])
+        code.append([len(code)+1, "input", len(inputData)])
 
         if variableName not in variables:
             variables.append([variableName, ""])
@@ -43,10 +43,9 @@ def varMod():
 printData = []
 
 def newPrint():
-    if runCode == False:
-        data = input("What do you whant to print:\n>> ")
-        printData.append(data)
-        code.append(["print", len(printData)])
+    data = input("What do you whant to print:\n>> ")
+    printData.append(data)
+    code.append([len(code)+1, "print", len(printData)])
 
 
 #input
@@ -69,10 +68,10 @@ def viewCode():
     print("////////////////CODE/////////////////\n")
 
     for x in code:
-        if x[0] == "print":
-            print(x[0] + "('"+printData[x[1]-1]+"')")
-        elif x[0] == "input":
-            print(inputData[x[1]-1][0] +" = "+ x[0] + "('"+inputData[x[1]-1][1]+"')")
+        if x[1] == "print":
+            print(x[1] + "('"+printData[x[2]-1]+"')")
+        elif x[1] == "input":
+            print(inputData[x[2]-1][0] +" = "+ x[1] + "('"+inputData[x[2]-1][1]+"')")
         else:
             print("")
 
@@ -85,43 +84,55 @@ def find(listToFind, dataToFind):
 
 #Run Code
 def runCode():
-    print("////////////////CODE/////////////////\n")
+    print("////////////////OUTPUT/////////////////\n")
     for x in code:
-        if x[0] == "print":
-            print(printData[x[1]-1])
-        elif x[0] == "input":
-            variables[find(variables, inputData[x[1]-1][0])][1] = input(inputData[x[1]-1][1]+"\n>> ")
+        if x[1] == "print":
+            print(printData[x[2]-1])
+        elif x[1] == "input":
+            variables[find(variables, inputData[x[2]-1][0])][1] = input(inputData[x[2]-1][1]+"\n>> ")
         else:
             print("")
 
-    print("\n//////////////CODE///////////////////")
+    print("\n//////////////OUTPUT///////////////////")
    
 #View Variables
 def viewVariables():
     print("////////////////VARIABLES/////////////////\n")
    
     for x in variables:
-        print(x[0]+" >>"+x[1]+"<<")
+        print(x[1]+" >>"+x[2]+"<<")
 
     print("\n//////////////VARIABLES///////////////////")
 
 
 def menu():
-    choice = input("(1) Print\n(2) Input\n(3) View Code\n(4) Run Code\n(5) View Variables\n(6) Save code\n>> ")
+    choice = input("(1) Code Edditing\n(2) Code Actions\n(3) Quit\n>> ")
     if choice == "1":
-        newPrint()
+        choice = input("(1) Print\n(2) Input\n>> ")
+        if choice == "1":
+            newPrint()
+        elif choice == "2":
+            newInput()
+        elif choice == "3":
+            quit)
+        else:
+            print("Error")
+
     elif choice == "2":
-        newInput()
-    elif choice == "3":
-        viewCode()
-    elif choice == "4":
-        runCode()
-    elif choice == "5":
-        viewVariables()
-    elif choice == "6":
-        saveCode()
+        choice = input("(1) View Code\n(2) Run Code\n(3) View Variables\n(4) Save code\n>> ")
+        if choice == "1":
+            viewCode()
+        elif choice == "2":
+            runCode()
+        elif choice == "3":
+            viewVariables()
+        elif choice == "4":
+            saveCode()
+        else:
+            print("Error")
     else:
         print("Error")
+
     menu()
 
 menu()
